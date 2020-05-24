28178499f8863a0241da692c7501dec0be068b85
Christian Rua <christian.rua@gmail.com>
2020-05-24 17:19:31 -0500

    assigment for the week 4

--- week4/src/main/scala/idealized/scala/week4.scala
+++ week4/src/main/scala/idealized/scala/week4.scala
@@ -15,7 +15,10 @@ abstract class Boolean {
 }
 
 object true extends Boolean {
-  def ifThenElse[T](t: => T, e: => T) = t
+
+import java.util.NoSuchElementException
+
+def ifThenElse[T](t: => T, e: => T) = t
 }
 
 object false extends Boolean {
@@ -48,3 +51,25 @@ class Succ(n: Nat) extends Nat {
   def - (that: Nat): Nat = if(that.isZero) this else n - that.predecessor
 }
 
+trait List[T] {
+  def isEmpty: Boolean
+
+  def head: T
+
+  def tail: List[T]
+}
+class Cons[T](val head: T, val tail:List[T]) extends List[T]{
+  def isEmpty = false
+}
+
+class Nil[T]() extends List[T]{
+  def isEmpty:Boolean = true
+  def head:Nothing = throw new NoSuchElementException("Nil.head")
+  def tail:Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+object List {
+
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
+}
